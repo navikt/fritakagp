9df87ab3606116d13a331c26b15ce1e6a16e14f3
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2025-06-24 10:55:43 +0200

    Bruk Aareg-klient som filtrerer ut ugyldige orgnr (#351)

--- src/main/kotlin/no/nav/helse/fritakagp/koin/ExternalSystemsModule.kt
+++ src/main/kotlin/no/nav/helse/fritakagp/koin/ExternalSystemsModule.kt
@@ -23,7 +23,6 @@ import no.nav.helsearbeidsgiver.pdl.PdlClient
 import no.nav.helsearbeidsgiver.utils.cache.LocalCache
 import org.koin.core.module.Module
 import org.koin.dsl.bind
-import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.minutes
 
 fun Module.externalSystemClients(env: Env) {
@@ -45,8 +44,8 @@ fun Module.externalSystemClients(env: Env) {
     single {
         val azureAuthClient: AuthClient = get()
         AaregClient(
-            url = env.aaregUrl,
-            cacheConfig = LocalCache.Config(7.days, 500),
+            baseUrl = env.aaregUrl,
+            cacheConfig = LocalCache.Config(5.minutes, 500),
             getAccessToken = azureAuthClient.fetchToken(IdentityProvider.AZURE_AD, env.scopeAareg)
         )
     } bind AaregClient::class
