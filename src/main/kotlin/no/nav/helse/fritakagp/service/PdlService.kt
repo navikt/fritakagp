d49724c0b7b683689ad90a259cb2c6b031058f48
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2025-07-03 10:29:38 +0200

    Bruk PDL-klient med cache, retry og timeout (#353)

--- src/main/kotlin/no/nav/helse/fritakagp/service/PdlService.kt
+++ src/main/kotlin/no/nav/helse/fritakagp/service/PdlService.kt
@@ -6,25 +6,11 @@ import no.nav.helsearbeidsgiver.pdl.PdlClient
 class PdlService(
     private val pdlClient: PdlClient
 ) {
-    fun hentNavn(ident: String): String {
-        return runBlocking { pdlClient.personNavn(ident) }
-            ?.fulltNavn().orEmpty()
-    }
+    fun hentNavn(ident: String): String =
+        runBlocking { pdlClient.personNavn(ident) }
+            ?.fulltNavn()
+            .orEmpty()
 
     fun hentAktoerId(ident: String): String? =
         runBlocking { pdlClient.hentAktoerID(ident) }
-
-    fun hentGeografiskTilknytning(ident: String): GeografiskTilknytning =
-        runBlocking { pdlClient.fullPerson(ident) }
-            .let {
-                GeografiskTilknytning(
-                    diskresjonskode = it?.diskresjonskode,
-                    geografiskTilknytning = it?.geografiskTilknytning
-                )
-            }
 }
-
-data class GeografiskTilknytning(
-    var diskresjonskode: String?,
-    var geografiskTilknytning: String?
-)
