0bcd7706217edadd55b996f892efcbe7a8e4d5f0
mortenbyhring <117746154+mortenbyhring@users.noreply.github.com>
2026-01-12 11:00:11 +0100

    fjernet ubrukte plugins og bumpet versjoner (#378)
    * fjernet ubrukte plugins:
    ben-manes og dependency-analysis
    
    * texas er enablet by default i nais nå, fjerner eksplisitt flagg
    
    * logback bump

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,13 +2,9 @@ pluginManagement {
     plugins {
         val kotlinVersion: String by settings
         val ktlintVersion: String by settings
-        val versionUpdatesVersion: String by settings
-        val dependencyAnalysisVersion: String by settings
 
         kotlin("jvm") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion
         id("org.jlleitschuh.gradle.ktlint") version ktlintVersion
-        id("com.github.ben-manes.versions") version versionUpdatesVersion
-        id("com.autonomousapps.dependency-analysis") version dependencyAnalysisVersion
     }
 }
