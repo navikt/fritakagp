63c0a5ec809999562dd9dd03a24259e14f106d3d
Jesper Hustad <jesper.forrest.hustad@nav.no>
2026-05-22 15:20:08 +0200

    Litt flere tester

--- src/test/kotlin/no/nav/helse/fritakagp/domain/KroniskSoeknadFravaerPerAarTest.kt
+++ src/test/kotlin/no/nav/helse/fritakagp/domain/KroniskSoeknadFravaerPerAarTest.kt
@@ -3,6 +3,7 @@ package no.nav.helse.fritakagp.domain
 import no.nav.helse.KroniskTestData
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class KroniskSoeknadFravaerPerAarTest {
 
@@ -23,5 +24,26 @@ class KroniskSoeknadFravaerPerAarTest {
         assertEquals(7F, result.first { it.aar == "2023" }.antallDager)
         assertEquals(7.5F, result.first { it.aar == "2024" }.antallDager)
         assertEquals(6F, result.first { it.aar == "2025" }.antallDager)
+    }
+
+    @Test
+    fun `fravaerPerAar med tomt set skal returnere tomt set`() {
+        val soeknad = KroniskTestData.soeknadKronisk.copy(fravaer = emptySet())
+        val result = soeknad.fravaerPerAar
+
+        assertTrue(result.isEmpty())
+    }
+
+    @Test
+    fun `fravaerPerAar med null dager skal returnere 0`() {
+        val fravaer = setOf(
+            FravaerData("2023-03", 0F),
+            FravaerData("2023-07", 0F)
+        )
+
+        val soeknad = KroniskTestData.soeknadKronisk.copy(fravaer = fravaer)
+        val result = soeknad.fravaerPerAar
+
+        assertEquals(setOf(FravaerPerAar("2023", 0F)), result)
     }
 }
